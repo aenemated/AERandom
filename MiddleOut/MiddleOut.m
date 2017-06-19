922341c4a521ea1db51e95dc56ed1b70ac010610
Trent Hamilton <aenemated@me.com>
2017-06-19 14:39:05 -0700

    Adding MiddleOut

---
+++ MiddleOut/MiddleOut.m
@@ -0,0 +1,48 @@
+#import <Foundation/Foundation.h>
+
+@interface MiddleOut : NSObject
++ (NSMutableArray*)buildMiddleOutPriorityWithSource:(NSArray*)sourceArray andCurrentIndex:(NSInteger)idx;
+@end
+
+@implementation MiddleOut
+
+
++ (NSMutableArray*)buildMiddleOutPriorityWithSource:(NSArray*)sourceArray andCurrentIndex:(NSInteger)idx {
+    
+    NSInteger pivot = idx;
+    NSMutableArray* ordered = [[NSMutableArray alloc] init];
+    
+    NSMutableArray* left = [[NSMutableArray alloc] init];
+    NSMutableArray* right = [[NSMutableArray alloc] init];
+    
+    for (int i=0;i<[sourceArray count];i++) {
+        if (i < pivot) {
+            [left addObject:[NSString stringWithFormat:@"%i",i]];
+        } else {
+            [right addObject:[NSString stringWithFormat:@"%i",i]];
+        }
+    }
+    
+    left = [[[left reverseObjectEnumerator] allObjects] mutableCopy];
+    
+    unsigned long mo;
+    if ([left count] > [right count]) {
+        mo = [left count];
+    } else {
+        mo = [right count];
+    }
+    
+    for (int ii=0;ii<mo;ii++) {
+        if (ii < [left count]) {
+            [ordered addObject:[left objectAtIndex:ii]];
+        }
+        
+        if (ii < [right count]) {
+            [ordered addObject:[right objectAtIndex:ii]];
+        }
+    }
+    
+    return ordered;
+}
+
+@end
